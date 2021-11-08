0d03a982fa7f877c41492a77edc57ba7df435860
Gayathri <nallamothugayathri8@gmail.com>
2021-11-08 13:50:52 +0530

    Gayathri

---
+++ main.py
@@ -0,0 +1,8 @@
+def check_palindrom(str1):
+         if str[::-1] == str1:
+                return TRUE
+str1 = input()
+if check_palindrome == True:
+       print("it is a palindrome")
+else:
+       print("not a palindrome")
